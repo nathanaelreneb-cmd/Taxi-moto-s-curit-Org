dc22f18d49cf0510c804cdf2b085f1e31bef9b7d
nathanaelreneb-cmd <nathanaelreneb@gmail.com>
2026-09-04 09:33:50 +0000

    Add files via upload

--- app/dashboard/MotoMap.js
+++ app/dashboard/MotoMap.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Tooltip, LayersControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
@@ -18,22 +18,32 @@ export default function MotoMap({ points }) {
   return (
     <MapContainer
       center={center}
-      zoom={points.length ? 13 : 7}
-      style={{ height: 360, width: '100%', borderRadius: 6 }}
+      zoom={points.length ? 16 : 7}
+      style={{ height: 420, width: '100%', borderRadius: 6 }}
     >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; OpenStreetMap contributors"
-      />
+      <LayersControl position="topright">
+        <LayersControl.BaseLayer checked name="Plan">
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; OpenStreetMap contributors"
+          />
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Satellite">
+          <TileLayer
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            attribution="Tiles &copy; Esri &mdash; Source: Esri, Maxar, Earthstar Geographics"
+          />
+        </LayersControl.BaseLayer>
+      </LayersControl>
       {points.map((p) => (
         <Marker key={p.uniqueId} position={[p.latitude, p.longitude]} icon={icon}>
-          <Popup>
+          <Tooltip permanent direction="top" offset={[0, -8]} className="moto-label">
             <b>{p.label}</b>
             <br />
             {p.speed != null ? `${Math.round(p.speed * 1.852)} km/h` : 'Vitesse inconnue'}
             <br />
             {p.fixTime ? new Date(p.fixTime).toLocaleString('fr-FR') : ''}
-          </Popup>
+          </Tooltip>
         </Marker>
       ))}
     </MapContainer>
